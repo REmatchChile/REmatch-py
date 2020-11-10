505d99bd6b89a142051b650a3de08562c5e3c79a
Nicolás Van Sint Jan <nicovsj@uc.cl>
2020-11-10 10:04:16 -0300

    python version lookup

--- config.sh
+++ config.sh
@@ -32,7 +32,7 @@ function pre_build {
 
   mkdir -pv REmatch/build && cd REmatch/build
 
-  cmake -DSWIG=true ..
+  cmake -DSWIG=true -DPYTHON_VERSION=$MB_PYTHON_VERSION ..
   cmake --build . --config Release
 
   tree ..
