225901de7277fe57228ddc43ce93386430c1e1e3
Nicolás Van Sint Jan <nicovsj@uc.cl>
2020-11-10 14:05:43 -0300

    build wheel commented

--- config.sh
+++ config.sh
@@ -9,13 +9,11 @@ function pre_build {
   python --version
   if [ -n "$IS_OSX" ]; then
     # brew update
-    brew install swig cmake boost tree
-    tree $PYTHON_ROOT
+    brew install swig cmake boost
     tree /Users/travis/build/REmatchChile/REmatch-py/venv
   else
     # SWIG depends on pcre and boost
-    yum install -y pcre-devel boost-devel tree
-    tree $PYTHON_ROOT
+    yum install -y pcre-devel boost-devel
     # Install SWIG
     curl -O -L http://downloads.sourceforge.net/swig/swig-4.0.2.tar.gz
     tar xzf swig-4.0.2.tar.gz
@@ -39,19 +37,21 @@ function pre_build {
 
 }
 
-function build_wheel {
-    # Set default building method to pip
-    build_bdist_wheel $@
-    # setup.py sdist fails with
-    # error: [Errno 2] No such file or directory: 'venv/lib/python3.5/_dummy_thread.py'
-    # for python less than 3.5
-    if [[ `python -c 'import sys; print(sys.version.split()[0] >= "3.6.0")'` == "True" ]]; then
-        python setup.py sdist --dist-dir $(abspath ${WHEEL_SDIR:-wheelhouse})
-    else
-        echo "skip sdist"
-    fi
-}
+# function build_wheel {
+#     # Set default building method to pip
+#     build_bdist_wheel $@
+#     # setup.py sdist fails with
+#     # error: [Errno 2] No such file or directory: 'venv/lib/python3.5/_dummy_thread.py'
+#     # for python less than 3.5
+#     if [[ `python -c 'import sys; print(sys.version.split()[0] >= "3.6.0")'` == "True" ]]; then
+#         python setup.py sdist --dist-dir $(abspath ${WHEEL_SDIR:-wheelhouse})
+#     else
+#         echo "skip sdist"
+#     fi
+# }
 
 function run_tests {
-  echo "Tests"
+  # Runs tests on installed distribution from an empty directory
+  python --version
+  python -c 'import sys; import pyrematch; sys.exit(0)
 }
