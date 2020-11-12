bc6319bfbb2e3da1f772437d7fb7f48c939795f1
Nicolás Van Sint Jan <nicovsj@uc.cl>
2020-11-12 15:23:31 -0300

    added echo to deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-tree .
+# tree .
 echo -e " ... running twine to deploy ... "
 pip install --upgrade twine
 if [ -n "$IS_OSX" ]; then
+  echo -e "upgrading pyOpenSSL ..."
   pip install --upgrade pyOpenSSL
 fi
 twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*.whl
