018e0173cc5a3a0942252601adafe2a68e369b2f
Nicolás Van Sint Jan <nicovsj@uc.cl>
2020-11-12 14:23:34 -0300

    install tree

--- config.sh
+++ config.sh
@@ -9,10 +9,10 @@ function pre_build {
   python --version
   if [ -n "$IS_OSX" ]; then
     # brew update
-    brew install swig cmake boost
+    brew install swig cmake boost tree
   else
     # SWIG depends on pcre and boost
-    yum install -y pcre-devel boost-devel
+    yum install -y pcre-devel boost-devel tree
     # Install SWIG
     curl -O -L http://downloads.sourceforge.net/swig/swig-4.0.2.tar.gz
     tar xzf swig-4.0.2.tar.gz
@@ -36,18 +36,18 @@ function pre_build {
 
 }
 
-# function build_wheel {
-#     # Set default building method to pip
-#     build_bdist_wheel $@
-#     # setup.py sdist fails with
-#     # error: [Errno 2] No such file or directory: 'venv/lib/python3.5/_dummy_thread.py'
-#     # for python less than 3.5
-#     if [[ `python -c 'import sys; print(sys.version.split()[0] >= "3.6.0")'` == "True" ]]; then
-#         python setup.py sdist --dist-dir $(abspath ${WHEEL_SDIR:-wheelhouse})
-#     else
-#         echo "skip sdist"
-#     fi
-# }
+function build_wheel {
+    # Set default building method to pip
+    build_bdist_wheel $@
+    # setup.py sdist fails with
+    # error: [Errno 2] No such file or directory: 'venv/lib/python3.5/_dummy_thread.py'
+    # for python less than 3.5
+    if [[ `python -c 'import sys; print(sys.version.split()[0] >= "3.6.0")'` == "True" ]]; then
+        python setup.py sdist --dist-dir $(abspath ${WHEEL_SDIR:-wheelhouse})
+    else
+        echo "skip sdist"
+    fi
+}
 
 function run_tests {
   # Runs tests on installed distribution from an empty directory
