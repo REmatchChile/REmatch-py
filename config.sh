fc3cacbbf0020877d5fd99df131fd6d34bd5393d
Nicolás Van Sint Jan <nicovsj@uc.cl>
2020-11-29 00:37:45 -0300

    add python3 libs on docker cmake

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ function pre_build {
   if [ -n "$IS_OSX" ]; then
     # brew update
     brew install swig boost tree
-    tree $PYTHON_ROOT
+    # tree $PYTHON_ROOT
   else
     # SWIG depends on pcre and boost
     yum install -y pcre-devel python-devel tree
@@ -19,7 +19,7 @@ function pre_build {
     gcc --version
 
     # Install SWIG
-    tree $PYTHON_ROOT/include
+    # tree $PYTHON_ROOT/include
     curl -O -L http://downloads.sourceforge.net/swig/swig-4.0.2.tar.gz
     tar xzf swig-4.0.2.tar.gz
     (cd swig-4.0.2 \
@@ -44,7 +44,7 @@ function pre_build {
   if [ -n "$IS_OSX" ]; then
     cmake -DSWIG=true -DPYTHON_VERSION=$MB_PYTHON_VERSION  ..
   else
-    cmake -DSWIG=true -DPython3_INCLUDE_DIRS=$PYTHON_ROOT/include ..
+    cmake -DSWIG=true -DPython3_INCLUDE_DIRS=$PYTHON_ROOT/include/python${MB_PYTHON_VERSION}m ..
   fi
   cmake --build . --config Release
 
