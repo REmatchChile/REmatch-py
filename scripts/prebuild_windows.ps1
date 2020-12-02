70aeeb7f2dea1f79521ff2988c08ca9e71d197f9
Nicolás Van Sint Jan <nicovsj@uc.cl>
2020-12-02 17:12:35 -0300

    add windows support github-actions

---
+++ scripts/prebuild_windows.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/env pwsh
+
+$PYTHON_VERSION = python --version | Select-String '^Python (\d\.\d).*' |
+                                     ForEach-Object{$_.Matches.Groups[1].Value}
+
+$BOOST_ROOT=Resolve-Path -Path "boost_1_74_0"
+
+Write-Host "BOOST_ROOT = $BOOST_ROOT"
+Write-Host "PYTHON_VERSION = $PYTHON_VERSION"
+
+New-Item -Path "REmatch/build" -ItemType Directory
+
+cd REmatch/build
+
+cmake -DSWIG=true -DPYTHON_VERSION="$PYTHON_VERSION" ..
+cmake --build . --config Release
+
+cd ../..
+
+Write-Host "ls REmatch/python/packages/pyrematch"
+Get-ChildItem REmatch/python/packages/pyrematch
---
+++ scripts/preset_windows.ps1
@@ -0,0 +1,10 @@
+#!/usr/bin/env pwsh
+
+choco install swig cmake
+
+$url = "https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.7z"
+$output = "boost_1_74_0.7z"
+
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+
+7z x boost_1_74_0.7z
