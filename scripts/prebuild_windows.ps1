eb4ce28a379ac7e403ebdc751d9838649280cb3f
Nicolás Van Sint Jan <nicovsj@uc.cl>
2020-12-02 18:46:09 -0300

    trying unix-like boost path

--- scripts/prebuild_windows.ps1
+++ scripts/prebuild_windows.ps1
@@ -3,7 +3,7 @@
 $PYTHON_VERSION = python --version | Select-String '^Python (\d\.\d).*' |
                                      ForEach-Object{$_.Matches.Groups[1].Value}
 
-$BOOST_ROOT=Resolve-Path -Path "boost_1_74_0"
+$BOOST_ROOT = (((Resolve-Path -Path "boost_1_74_0") -replace "\\","/") -replace ":","").ToLower().Trim("/")
 
 Write-Host "BOOST_ROOT = $BOOST_ROOT"
 Write-Host "PYTHON_VERSION = $PYTHON_VERSION"
@@ -12,7 +12,7 @@ New-Item -Path "REmatch/build" -ItemType Directory
 
 cd REmatch/build
 
-cmake -DSWIG=true -DPYTHON_VERSION="$PYTHON_VERSION" -DBoost_INCLUDE_DIR="$BOOST_ROOT\boost" ..
+cmake -DSWIG=true -DPYTHON_VERSION="$PYTHON_VERSION" -DBoost_INCLUDE_DIR="$BOOST_ROOT/boost" ..
 cmake --build . --config Release
 
 cd ../..
