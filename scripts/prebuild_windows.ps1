22f225c58ef4c3e60440c9bbae98e9b0e60afdef
Oscars <oscar.carcamoz@uc.cl>
2021-04-09 16:34:22 -0400

    Using appveyor commands

--- scripts/prebuild_windows.ps1
+++ scripts/prebuild_windows.ps1
@@ -4,7 +4,7 @@ $PYTHON_VERSION = python --version | Select-String '^Python (\d\.\d).*' |
                                      ForEach-Object{$_.Matches.Groups[1].Value}
 
 # $BOOST_ROOT = (((Resolve-Path -Path "boost_1_74_0") -replace "\\","/") -replace ":","").ToLower().Trim("/")
-$BOOST_ROOT = $BOOST_ROOT_1_72_0
+$BOOST_CHOCO_ROOT = C:\Program Files\boost\boost_1_54_0
 
 Write-Host "BOOST_ROOT = $BOOST_ROOT"
 Write-Host "BOOST_ROOT_1_72_0 = $BOOST_ROOT_1_72_0"
@@ -17,7 +17,11 @@ cd REmatch/build
 cmake -DSWIG=true -DPYTHON_VERSION="$PYTHON_VERSION" ..
 cmake --build . --config Release
 
-cd ../..
+cd ..
 
 Write-Host "ls REmatch/python/packages/pyrematch"
 Get-ChildItem REmatch/python/packages/pyrematch
+
+Copy-Item "python\packages\pyrematch\Release\_rematch.pyd" -Destination "python\packages\pyrematch"
+
+python setup.py bdist_wheel
