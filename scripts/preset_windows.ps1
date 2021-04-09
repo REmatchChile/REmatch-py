f1076e17b3f0cb5a706107f8a62cdab74ebcd5d0
Oscars <oscar.carcamoz@uc.cl>
2021-04-09 14:39:21 -0400

    Trying windows

--- scripts/preset_windows.ps1
+++ scripts/preset_windows.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 
-choco install swig cmake --no-progress
+choco install swig cmake boost-msvc-14.2 --no-progress
 
 # $url = "https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.7z"
 # $output = "boost_1_74_0.7z"
