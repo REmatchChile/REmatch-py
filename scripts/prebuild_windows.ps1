e6e2ebe4c073c175de042b634825cfa2c4d3776c
Nicolás Van Sint Jan <nicovsj@uc.cl>
2021-05-01 22:26:13 -0400

    try setting arch in cmake

--- scripts/prebuild_windows.ps1
+++ scripts/prebuild_windows.ps1
@@ -8,14 +8,21 @@ $PYTHON_VERSION = python --version | Select-String '^Python (\d\.\d).*' |
 
 $BOOST_ROOT = (Resolve-Path -Path "boost_1_74_0")
 
+if ([Environment]::Is64BitOperatingSystem) {
+  $PYTHON_ARCH = "x64"
+} else {
+  $PYTHON_ARCH = "Win32"
+}
+
 Write-Host "BOOST_ROOT = $BOOST_ROOT"
 Write-Host "PYTHON_VERSION = $PYTHON_VERSION"
+Write-Host "PYTHON_ARCH = $PYTHON_VERSION"
 
 New-Item -Path "REmatch/build" -ItemType Directory
 
 cd REmatch/build
 
-cmake -DSWIG=true -DPYTHON_VERSION="$PYTHON_VERSION" -DBOOST_ROOT="$BOOST_ROOT" ..
+cmake -A "$PYTHON_ARCH" -DSWIG=true -DPYTHON_VERSION="$PYTHON_VERSION" -DBOOST_ROOT="$BOOST_ROOT" ..
 cmake --build . --config Release
 
 cd ..
